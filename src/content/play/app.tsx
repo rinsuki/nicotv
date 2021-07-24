a9e9581c32ee4874418fe255172d5dce899c5102
rinsuki <428rinsuki+git@gmail.com>
2021-07-25 01:39:19 +0900

    スレッドIDが指定されていない時は動画から取るように

--- src/content/play/app.tsx
+++ src/content/play/app.tsx
@@ -9,22 +9,23 @@ import { VideoPlayerState } from "./video-player/state";
 const VideoPlayer: React.FC<{id: string, thread?: string, forks: number[], videoSrc?: string}> = props => {
     const [info, setInfo] = useState<any>()
     const [playerState] = useState(() => new VideoPlayerState())
+    const [thread, setThread] = useState(() => props.thread)
     useEffect(() => {
         fetch(`https://www.nicovideo.jp/api/watch/v3_guest/${props.id}?_frontendId=${FRONTEND_ID}&_frontendVersion=0&actionTrackId=${generateActionTrackID()}&isContinueWatching=true`)
             .then(r => r.json())
             .then(r => {
                 console.log("nicovideo watch res", r)
                 setInfo(r.data)
+                setThread(current => current != null ? current : r.data.comment.threads.filter((a: any) => a.isActive)[0].id)
             })
     }, [])
     if (info == null) return null
-    const thread = props.thread != null ? parseInt(props.thread) : 1558794783
     const vsrc = props.videoSrc
 
     return <div className="player">
         <title>{info.video.title} ({info.video.id}) - nicotv </title>
         {vsrc != null ? <SimpleVideoPlayer src={vsrc} playerState={playerState} /> : <DMCVideoPlayer delivery={info.media.delivery} playerState={playerState} />}
-        <ZenzaCommentRenderer thread={thread} forks={props.forks} duration={300} playerState={playerState}/>
+        {thread != null && <ZenzaCommentRenderer thread={thread} forks={props.forks} duration={300} playerState={playerState}/>}
     </div>
 }
 
