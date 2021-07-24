8c290ce84e161db21ff327a9af285a207ce8cadc
rinsuki <428rinsuki+git@gmail.com>
2021-07-25 02:24:58 +0900

    ピン留め機能実装 (デバッグ用)

--- src/content/top/index.ts
+++ src/content/top/index.ts
@@ -2,5 +2,41 @@ const form = document.querySelector("form")!
 form.addEventListener("submit", e => {
     e.preventDefault()
     const a = (Array.from(new FormData(form).entries()) as [string, string][]).filter(a => a[1].length)
-    open("play.html?" + new URLSearchParams(a), "_blank")
+    open("play.html?" + new URLSearchParams(a), "player")
 })
+
+document.getElementById("addpinbutton")?.addEventListener("click", () => {
+    const a = (Array.from(new FormData(form).entries()) as [string, string][]).filter(a => a[1].length)
+    localStorage.setItem("pinned_list", JSON.stringify([
+        ...fallbackableJSONParse("pinned_list", []),
+        a,
+    ]))
+    reloadPinnedList()
+})
+
+function fallbackableJSONParse<T>(input: string | undefined | null, fallback: T) {
+    try {
+        if (input == null) return fallback
+        return JSON.parse(input)
+    } catch(e) {
+        return fallback
+    }
+}
+
+function reloadPinnedList() {
+    const ul = document.getElementById("pinned_list")
+    if (ul == null) return
+    while (ul.children.length) ul.children.item(0)?.remove()
+    const list = fallbackableJSONParse(localStorage.getItem("pinned_list"), [])
+    for (const pin of list) {
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+        a.href = `play.html?${new URLSearchParams(pin)}`
+        a.target = "player"
+        a.innerText = new URLSearchParams(pin).toString()
+        li.appendChild(a)
+        ul.appendChild(li)
+    }
+}
+
+reloadPinnedList()
